1ebad9842c52456ad91aea57336b3f75e85ab700
Christian Frugard <frugardc@gmail.com>
2013-02-13 10:01:04 -0500

    rough structure

--- application.rb
+++ application.rb
@@ -7,3 +7,23 @@ end
 get '/about' do 
 	erb :about
 end
+
+get '/bios' do 
+	erb :bios
+end
+
+get '/mission' do 
+	erb :mission
+end
+
+get '/history' do 
+	erb :history
+end
+
+get '/namesake' do 
+	erb :namesake
+end
+
+get '/sponsorship' do 
+	erb :sponsorship
+end
